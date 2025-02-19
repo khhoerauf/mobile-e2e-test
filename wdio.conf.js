318b2e7f6ff5d04f8532426278b5dfbdd781a734
kingahoerauf <kingahoerauf@gmail.com>
2025-02-19 10:13:43 +0100

    feat: support multiple Android devices and integrate iOS device configuration

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,101 +1,29 @@
 //const path = require("path");
 const os = require("os");
+const {
+  setUpIosDevices,
+  setUpAndroidDevices,
+} = require("./e2e/support/config.js");
 
 exports.config = {
   runner: "local",
   port: 4723,
   specs: ["./e2e/specs/**-test.js"],
-  exclude: ["./e2e/specs/start-workflow-test.js"],
+  exclude: [],
   maxInstances: parseInt(process.env.MAX_INSTANCES),
-  capabilities: [
-    {
-      platformName: "Android",
-      "appium:deviceName": process.env.DEVICE_NAME,
-      "appium:automationName": "UiAutomator2",
-      "appium:appPackage": process.env.APP_PACKAGE,
-      "appium:appActivity": process.env.APP_ACTIVITY,
-      //"appium:app": path.join(process.cwd(), "app/Monefy.apk"),
-    },
-    {
-      platformName: "iOS",
-      "appium:udid": process.env.UUID,
-      "appium:automationName": "XCUITest",
-      "appium:bundleId": process.env.BUNDLE_ID,
-      "appium:includeSafariInWebviews": true,
-      "appium:showIOSLog": true
-      //"appium:app": path.join(process.cwd(), "app/Monefy.apk"),
-    }
-  ],
-  // ===================
-  // Test Configurations
-  // ===================
-  // Define all options that are relevant for the WebdriverIO instance here
-  // Level of logging verbosity: trace | debug | info | warn | error | silent
+  capabilities: [],
   logLevel: "info",
-  //
-  // Set specific log levels per logger
-  // loggers:
-  // - webdriver, webdriverio
-  // - @wdio/browserstack-service, @wdio/lighthouse-service, @wdio/sauce-service
-  // - @wdio/mocha-framework, @wdio/jasmine-framework
-  // - @wdio/local-runner
-  // - @wdio/sumologic-reporter
-  // - @wdio/cli, @wdio/config, @wdio/utils
-  // Level of logging verbosity: trace | debug | info | warn | error | silent
-  // logLevels: {
-  //     webdriver: 'info',
-  //     '@wdio/appium-service': 'info'
-  // },
-  //
-  // If you only want to run your tests until a specific amount of tests have failed use
-  // bail (default is 0 - don't bail, run all tests).
   bail: 0,
-  //
-  // Set a base URL in order to shorten url command calls. If your `url` parameter starts
-  // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
-  // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
-  // gets prepended directly.
-  // baseUrl: 'http://localhost:8080',
-  //
-  // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
-  //
-  // Default timeout in milliseconds for request
-  // if browser driver or grid doesn't send response
   connectionRetryTimeout: 120000,
-  //
-  // Default request retries count
   connectionRetryCount: 3,
-  //
-  // Test runner services
-  // Services take over a specific job you don't want to take care of. They enhance
-  // your test setup with almost no effort. Unlike plugins, they don't add new
-  // commands. Instead, they hook themselves up into the test process.
   services: ["appium"],
-
-  // Framework you want to run your specs with.
-  // The following are supported: Mocha, Jasmine, and Cucumber
-  // see also: https://webdriver.io/docs/frameworks
-  //
-  // Make sure you have the wdio adapter package for the specific framework installed
-  // before running any tests.
+  appium: {
+    args: {
+      "--log": "./logs/",
+    },
+  },
   framework: "mocha",
-
-  //
-  // The number of times to retry the entire specfile when it fails as a whole
-  // specFileRetries: 1,
-  //
-  // Delay in seconds between the spec file retry attempts
-  // specFileRetriesDelay: 0,
-  //
-  // Whether or not retried spec files should be retried immediately or deferred to the end of the queue
-  // specFileRetriesDeferred: false,
-  //
-  // Test reporter for stdout.
-  // The only one supported by default is 'dot'
-  // see also: https://webdriver.io/docs/dot-reporter
-  // Options to be passed to Mocha.
-  // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: "bdd",
     timeout: 60000,
@@ -127,8 +55,35 @@ exports.config = {
    * @param {object} config wdio configuration object
    * @param {Array.<Object>} capabilities list of capabilities details
    */
-  // onPrepare: function (config, capabilities) {
-  // },
+  onPrepare: async function (config, capabilities, specs, cid) {
+    androidDevices = await setUpAndroidDevices();
+    iosDevices = await setUpIosDevices();
+
+    if (androidDevices) {
+      androidDevices.forEach((device) => {
+        capabilities.push({
+          "appium:automationName": "UiAutomator2",
+          "appium:platformName": "Android",
+          "appium:udid": device.udid,
+          "appium:appPackage": process.env.ANDROID_APP_PACKAGE,
+          "appium:appActivity": process.env.ANDROID_APP_ACTIVITY,
+          "appium:noReset": false, //Appium driver to avoid its usual reset logic during session start and cleanup
+          //"appium:app": path.join(process.cwd(), "app/Monefy.apk"),
+        });
+      });
+    }
+
+    if (iosDevices) {
+      iosDevices.forEach((device) => {
+        capabilities.push({
+          "appium:automationName": "XCUITest",
+          "appium:platformName": "iOS",
+          "appium:udid": device.udid,
+          "appium:bundleId": process.env.BUNDLE_ID,
+        });
+      });
+    }
+  },
   /**
    * Gets executed before a worker process is spawned and can be used to initialize specific service
    * for that worker as well as modify runtime environments in an async fashion.
